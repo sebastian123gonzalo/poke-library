385728f39c58972fe552f8027cd40cf3962e45fc
sebastian123gonzalo <sebastian123gonzalo@gmail.com>
2025-03-21 21:17:53 -0500

    feat: state loading added

--- poke-library/src/app/page.tsx
+++ poke-library/src/app/page.tsx
@@ -6,11 +6,14 @@ import { Pokemon } from "./utils/models/pokemon";
 
 export default function Home() {
   const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const getPokemon = async () => {
+      setIsLoading(true);
       const data = await fetchAllPokemon();
       setPokemonList(data);
+      setIsLoading(false);
     };
     getPokemon();
   }, []);
@@ -18,6 +21,14 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gray-900 text-white flex flex-col items-center">
       <h1 className="text-4xl font-bold my-8">Pokédex</h1>
+
+      {
+        isLoading && (
+        <div className="flex justify-center items-center h-64">
+          <div className="w-16 h-16 border-4 border-gray-300 border-t-transparent rounded-full animate-spin"></div>
+        </div> )
+      }
+
       <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-6">
         {pokemonList.map((pokemon) => (
           <div key={pokemon.id} className="bg-gray-800 p-4 rounded-lg shadow-lg flex flex-col items-center">
