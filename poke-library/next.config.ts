de58df325193bd9a3dcc2108af204e50af5e0fda
sebastian123gonzalo <sebastian123gonzalo@gmail.com>
2025-03-23 10:22:44 -0500

    fix: NextConfig updated

--- poke-library/next.config.ts
+++ poke-library/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Ensures static site generation
+  trailingSlash: true,
 };
 
 export default nextConfig;
